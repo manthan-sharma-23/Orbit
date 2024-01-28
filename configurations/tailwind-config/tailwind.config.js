c4b911436ef6f88a63d23aab44d38f2319186629
Manthan Sharma <143496678+manthan-sharma-23@users.noreply.github.com>
2024-01-28 22:48:00 +0530

    configs

---
+++ configurations/tailwind-config/tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    content: [
+        '../../packages/ui/components/**/*.{ts,tsx}',
+        './src/**/*.{ts,tsx,js,jsx}',
+        './app/**/*.{tr,tsx,js,jsx}'
+    ],
+    theme: {
+        extend: {}
+    },
+    plugins: []
+}
---
+++ packages/services/src/index.ts
@@ -0,0 +1,5 @@
+export function main() {
+  console.log("Hello from main services");
+
+  return "Hello from main services";
+}
