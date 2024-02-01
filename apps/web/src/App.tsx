2419fdf2704fc82de7b733f53d548448921b4b49
Manthan Sharma <143496678+manthan-sharma-23@users.noreply.github.com>
2024-02-01 16:54:08 +0530

    code migrate from nextjs --> react :: client-server storage issues

---
+++ apps/web/src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="h-screen w-screen bg-black flex justify-center items-center text-4xl text-white">
+      Hello
+    </div>
+  );
+}
+
+export default App;
