fdb3dfe91410ff9cc6397dc3a1183498ec43f5c6
Manthan Sharma <143496678+manthan-sharma-23@users.noreply.github.com>
2024-02-05 21:27:06 +0530

    rtkq hooks ::-->> thunk

--- apps/web/src/components/containers/ChatProfilePannel.tsx
+++ apps/web/src/components/containers/ChatProfilePannel.tsx
@@ -4,9 +4,11 @@ import * as Ri from "react-icons/ri";
 import { BsPerson } from "react-icons/bs";
 import { FiPlus } from "react-icons/fi";
 import { useState } from "react";
+import { useGetFriendsQuery } from "../../lib/store/rtk-query/friends.api";
 
-const ChatProfilePannel = () => {
+const ChatProfilePannel = async () => {
   const [isAddOn, setAddOn] = useState<boolean>(false);
+  const friends = await useGetFriendsQuery();
 
   return (
     <div className="h-full w-full bg-[#F0F0F0] flex flex-col justify-start items-center">
@@ -44,53 +46,32 @@ const ChatProfilePannel = () => {
           </div>
         </div>
         <div className="h-full w-4/5 relative bg-transparent overflow-y-scroll scrollw pl-2 pr-[2px]">
-          <ProfileCard />
+          {friends.data &&
+            friends.data.friends.map((friend:{name:string,image:string}) => (
+              <ProfileCard name={friend.name} image={friend.image} />
+            ))}
         </div>
       </div>
     </div>
   );
 };
 
-const ProfileCard = () => {
+const ProfileCard = ({ name, image }: { name?: string; image?: string }) => {
   return (
     <>
       <div className="bg-white h-[8.5vh] w-full rounded-lg p-2 flex items-center my-2 cursor-pointer shadow-lg">
         <div className="h-full w-[20%] flex justify-center items-center rounded-full overflow-hidden">
-          <img
-            src="https://img.freepik.com/free-photo/portrait-man-laughing_23-2148859448.jpg?size=338&ext=jpg&ga=GA1.1.1448711260.1706918400&semt=ais"
-            className="h-[60px] w-[60px] rounded-full"
-          />
+          <img src={image} className="h-[60px] w-[60px] rounded-full" />
         </div>
         <div className="h-full w-[90%] flex justify-center items-center">
           <div className="h-full w-[90%] flex flex-col justify-center items-start pl-3">
-            <div className="font-mono font-[800]">Dakota Johnson</div>
+            <div className="font-mono font-[800]">{name}</div>
             <div className="font">
               <p>Hey There Have you Reqached ??</p>
             </div>
           </div>
           <div className="h-full w-[10%] flex justify-center items-center">
             <span className="h-[25px] font-roboto w-[25px] flex justify-center items-center bg-gray-500 rounded-full text-white">
-              1
-            </span>
-          </div>
-        </div>
-      </div>
-      <div className="bg-white h-[8.5vh] w-full rounded-lg p-2 flex items-center my-2 cursor-pointer shadow-lg">
-        <div className="h-full w-[20%] flex justify-center items-center rounded-full overflow-hidden">
-          <img
-            src="https://img.freepik.com/free-photo/portrait-man-laughing_23-2148859448.jpg?size=338&ext=jpg&ga=GA1.1.1448711260.1706918400&semt=ais"
-            className="h-[60px] w-[60px] rounded-full"
-          />
-        </div>
-        <div className="h-full w-[90%] flex justify-center items-center">
-          <div className="h-full w-[90%] flex flex-col justify-center items-start pl-3">
-            <div className="font-mono font-[800]">Dakota Johnson</div>
-            <div className="font">
-              <p>Hey There Have you Reqached ??</p>
-            </div>
-          </div>
-          <div className="h-full w-[10%] flex justify-center items-center">
-            <span className="h-[25px] font-roboto w-[25px] flex justify-center items-center bg-blue-500 rounded-full text-white">
               1
             </span>
           </div>
