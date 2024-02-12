e39a53ac7cad473e7c2eab720832d8667ccabc71
Manthan Sharma <143496678+manthan-sharma-23@users.noreply.github.com>
2024-02-12 22:36:13 +0530

    ui

--- apps/web/src/components/container/Render.tsx
+++ apps/web/src/components/container/Render.tsx
@@ -3,7 +3,6 @@ import { useGetUser } from "../../features/hooks/useGetUser.hook";
 
 function Render({ children }: { children: React.ReactNode }) {
   useGetUser();
-
   return <div>{children}</div>;
 }
 
--- apps/web/src/components/layouts/AppLayout.tsx
+++ apps/web/src/components/layouts/AppLayout.tsx
@@ -1,9 +1,16 @@
 import { Outlet } from "react-router-dom";
+import bg from "../../assets/bg.jpg";
+import "../../styles/globals.css";
 
 const AppLayout = () => {
   return (
-    <div className="text-black h-screen w-screen border-">
-      Hello
+    <div className="text-black h-screen w-screen overflow-hidden flex justify-center bg-black items-center">
+      <img src={bg} className="bg-image h-full w-full" />
+      <div className="h-full w-full bg-transparent p-3">
+        <div className="h-full w-[6vw] bg-white/40 rounded-2xl flex justify-start flex-col items-center p-1">
+          Hello
+        </div>
+      </div>
       <Outlet />
     </div>
   );
